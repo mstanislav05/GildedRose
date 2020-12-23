201802a0095a19c88a7af757de981a4517bc1d42
mstanislav05 <43286187+mstanislav05@users.noreply.github.com>
2020-12-23 08:19:31 -0600

    Add files via upload
    Manually upload project due to VS 2019 and GitHub credential error on push attempts

---
+++ GildedRose/GildedRose/GildedRose.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Mikaela Stanislav / Technical Showcase for Lean Techniques
+// December 2020.  mstanislav05@gmail.com
+
+namespace GildedRose
+{
+    class GildedRose
+    {
+        static void Main(string[] args)
+        {
+            List<Item> itemList = new List<Item>();
+            List<Item> itemChoices = new List<Item>();
+            itemChoices.Add(new Item("Aged Brie", 0, 0, false));
+            itemChoices.Add(new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0, false));
+            itemChoices.Add(new Item("Sulfuras, Hand of Ragnaros", 0, 0, false));
+            itemChoices.Add(new Item("Winter Cap", 0, 0, false));
+            itemChoices.Add(new Item("Greatsword", 0, 0, false));
+
+            Console.WriteLine("Please enter how many days to run");
+            string dayInput = Console.ReadLine();
+            int runDaysNum;
+            bool result = int.TryParse(dayInput, out runDaysNum);
+            while (result != true)
+            {
+                Console.WriteLine("Please enter how many days to run (must be numeric)");
+                dayInput = Console.ReadLine();
+                result = int.TryParse(dayInput, out runDaysNum);
+            }
+
+            for (int day = 1; day <= runDaysNum; day++)
+            {
+                if (day == 1)
+                {
+                    //Ask user to stock starting inventory
+                    Console.WriteLine("Please enter item number to add to starting inventory. Enter -1 to finish.");
+                    foreach (var itemChoice in itemChoices)
+                    {
+                        Console.WriteLine(("[" + (itemChoices.IndexOf(itemChoice) + 1) + "]" + itemChoice.name));
+                    }
+                    bool validChoice = int.TryParse(Console.ReadLine(), out int inputNum);
+                    while (validChoice != true)
+                    {
+                        Console.WriteLine("Please enter item number to add to starting inventory. Entry must be numeric. Enter -1 to finish.");
+                        foreach (var itemChoice in itemChoices)
+                        {
+                            Console.WriteLine(("[" + (itemChoices.IndexOf(itemChoice) + 1) + "]" + itemChoice.name));
+                        }
+                        validChoice = int.TryParse(Console.ReadLine(), out inputNum);
+                    }
+                    while (inputNum != -1)
+                    {
+                        if (inputNum > 0 && inputNum <= itemChoices.Count)
+                        {
+                            Item chosenItem = itemChoices[inputNum - 1];
+                            Item newItem;
+                            if (chosenItem.name != "Sulfuras, Hand of Ragnaros")
+                            {
+                                int sellinDays;
+                                int qualityStart;
+                                int conjuredNum;
+                                bool conjuredStatus = false;
+
+                                Console.WriteLine("Please enter sell in days for new item '" + chosenItem.name + "'");
+                                while (int.TryParse(Console.ReadLine(), out sellinDays) == false || sellinDays < 0)
+                                {
+                                    Console.WriteLine("Please enter sell in days for new item '" + chosenItem.name + "' (minimum of 0)");
+                                }
+
+                                Console.WriteLine("Please enter starting quality for new item '" + chosenItem.name + "'");
+                                while (int.TryParse(Console.ReadLine(), out qualityStart) == false || qualityStart > 50 || qualityStart < 0)
+                                {
+                                    Console.WriteLine("Please enter starting quality for new item '" + chosenItem.name + "' (maximum of 50)");
+                                }
+
+                                Console.WriteLine("Please enter Conjured status (0 = false, 1 = true)");
+                                while (int.TryParse(Console.ReadLine(), out conjuredNum) == false || (conjuredNum != 0 && conjuredNum != 1))
+                                {
+                                    Console.WriteLine("Please enter Conjured status (0 = false, 1 = true)");
+                                }
+                                if (conjuredNum == 1)
+                                {
+                                    conjuredStatus = true;
+                                }
+
+                                newItem = new Item(chosenItem.name, sellinDays, qualityStart, conjuredStatus);
+                            }
+                            else
+                            {
+                                //Do not request custom settings- defaults used for Sulfuras, Hand of Ragnaros
+                                newItem = new Item(chosenItem.name, 0, 80, false);
+                                Console.WriteLine("Sulfuras, Hand of Ragnaros successsfully added to inventory.");
+                            }
+                            itemList.Add(newItem);
+                        }
+                        Console.WriteLine("Please enter item number to add to starting inventory. Enter -1 to finish.");
+                        foreach (var itemChoice in itemChoices)
+                        {
+                            Console.WriteLine(("[" + (itemChoices.IndexOf(itemChoice) + 1) + "]" + itemChoice.name));
+                        }
+                        _ = int.TryParse(Console.ReadLine(), out inputNum);
+                    }
+                    Console.WriteLine("Welcome to the Gilded Rose! \nHere is a list of our current inventory:");
+                }
+
+                //Display Inventory and stats for each day
+                Console.WriteLine("Day " + day);
+                foreach (var item in itemList)
+                {
+                    Console.WriteLine(item.name);
+                    Console.WriteLine("\tSell Days Remaining: " + item.sell_in);
+                    Console.WriteLine("\tQuality: " + item.quality);
+                    Console.WriteLine("\tConjured? " + item.conjuredInd);
+                }
+                Console.WriteLine("Press any key to advance a day");
+                Console.ReadLine();
+
+                foreach (Item item in itemList)
+                {
+                    item.update_quality(item);
+                }
+            }
+        }
+    }
+}
---
+++ GildedRose/GildedRose/Item.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// Mikaela Stanislav / Technical Showcase for Lean Techniques
+// December 2020.  mstanislav05@gmail.com
+
+namespace GildedRose
+{
+    class Item
+    {
+        public string name { get; set; }
+        public int sell_in { get; set; }
+        public int quality { get; set; }
+        public bool conjuredInd { get; set; }
+
+        //Constructors
+        public Item()
+        {
+            this.name = "";
+            this.sell_in = 0;
+            this.quality = 0;
+            this.conjuredInd = false;
+        }
+
+        public Item(string inName, int inSellIn, int inQuality, bool inConjuredInd)
+        {
+            this.name = inName;
+            this.sell_in = inSellIn;
+            this.quality = inQuality;
+            this.conjuredInd = inConjuredInd;
+        }
+
+        public void update_quality(Item inItem)
+        {
+            inItem.sell_in--;
+            //Updates the sell in days and quality according to item sent in
+            if (inItem.quality > 0 && inItem.quality < 50)
+            {
+                int conjuredFactor = 1;
+                if(inItem.conjuredInd == true)
+                {
+                    conjuredFactor = 2;
+                }
+                switch (inItem.name)
+                {
+                    case "Aged Brie":
+                        inItem.quality++;
+                        if (inItem.sell_in <= 0)
+                        {
+                            inItem.quality = inItem.quality - 2;
+                        }
+                        break;
+                    case "Backstage passes to a TAFKAL80ETC concert":
+                        if (inItem.sell_in <= 0)
+                        {
+                            inItem.quality = 0;
+                        }
+                        else if (inItem.sell_in <= 10 && inItem.sell_in > 5)
+                        {
+                            inItem.quality = inItem.quality + 2;
+                        }
+                        else if (inItem.sell_in <= 5)
+                        {
+                            inItem.quality = inItem.quality + 3;
+                        }
+                        break;
+                    case "Sulfuras, Hand of Ragnaros":
+                        inItem.sell_in++; //undo above sell days decrement
+                        break;
+                    default:
+                        inItem.quality = inItem.quality - (1 * conjuredFactor);
+                        if (inItem.sell_in < 0)
+                        {
+                            inItem.quality = inItem.quality - (1 * conjuredFactor); //degrade twice as fast after sell date
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
